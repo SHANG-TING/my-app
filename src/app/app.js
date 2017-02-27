226d2397149af1a7768228e044f9cb9e04fe4c35
SHANG-TING <rebom8888@gmail.com>
2017-02-27 14:21:36 +0800

    改變url

--- src/app/app.js
+++ src/app/app.js
@@ -12,7 +12,7 @@ let app = () => {
 
 class AppCtrl {
   constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
+    this.url = 'https://github.com/SHANG-TING/my-app';
   }
 }
 
